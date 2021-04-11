596a34d0e4fc2b3f06c1e83584663e75b2269a34
Justus Keinänen <justuskeinanen@gmail.com>
2021-04-11 18:05:33 +0300

    Messages with database, according the instructions

--- app.py
+++ app.py
@@ -3,19 +3,34 @@ from flask import redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://jiikeina"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///jiikeina"
 db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
+    result = db.session.execute("SELECT COUNT(*) FROM messages")
+    count = result.fetchone()[0]
+    result = db.session.execute("SELECT content FROM messages")
+    messages = result.fetchall()
+    return render_template("index.html", count = count, messages = messages)
     
-    friends = ["Riikka", "Justus", "Kirsi", "Sami", "J-P"]
-    return render_template("index.html", message = "Friends List: ", items= friends)
+@app.route("/new")
+def new():
+    return render_template("new.html")
 
-@app.route("/form")
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    sql = "INSERT INTO messages (content) VALUES (:content)" 
+    db.session.execute(sql, {"content":content})
+    db.session.commit()
+    return redirect("/")
+
+
+""" @app.route("/form")
 def form():
     return render_template("form.html")
 
 @app.route("/result", methods=["POST"])
 def result():
-    return render_template("result.html", name= request.form["name"])
+    return render_template("result.html", name= request.form["name"]) """
